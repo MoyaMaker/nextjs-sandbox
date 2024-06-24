5f5a2b10d883892244c2ac7e321d5989cf5bcabe
Jonathan <j.moya.jt@gmail.com>
2024-06-23 19:11:48 -0500

    form steps

--- app/form/submit-button.tsx
+++ app/form/submit-button.tsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react";
 import { SubmitHandler, useFormContext } from "react-hook-form";
+
+type UnknownData = {
+  ID: string;
+  [key: string]: any;
+};
 
 const actions = [
   {
@@ -9,16 +15,60 @@ const actions = [
   },
 ];
 
+const createData = (data: UnknownData): Promise<Response> =>
+  new Promise((resolve) => {
+    const body = JSON.stringify({
+      ...data,
+      ID: "1",
+    });
+
+    const response = new Response(body, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTimeout(() => {
+      resolve(response);
+    }, 1500);
+  });
+
 export function SubmitButton() {
-  const { handleSubmit } = useFormContext();
+  const { handleSubmit } = useFormContext<UnknownData>();
+
+  const [step, setStep] = useState<number | undefined>();
+
+  const [saveData, setSaveData] = useState<UnknownData | undefined>();
+
+  const nextStep = () => {
+    setStep((prevStep) => {
+      if (prevStep !== undefined && prevStep < actions.length - 1) {
+        return prevStep! + 1;
+      }
+    });
+  };
+
+  const onSubmit: SubmitHandler<UnknownData> = async (data) => {
+    console.log("data", data);
+    const saveData = await createData(data);
+    const json = await saveData.json();
+    setSaveData(json);
+  };
 
   const handleAction = () => {
-    handleSubmit(onSubmit)();
+    if (actions) {
+      console.log("Set first step");
+      setStep(0);
+    }
   };
 
-  const onSubmit: SubmitHandler<any> = async (data) => {
-    console.log("data", data);
-  };
+  useEffect(() => {
+    if (step !== undefined) {
+      console.log("Step", step);
+      console.log(actions[step].type);
+    }
+  }, [step]);
 
   return (
     <button
